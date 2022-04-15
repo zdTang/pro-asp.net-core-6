7ed1a0728019e9efe294a075f27af9ca948cf074
Zhendong Tang <zhendongtt@gmail.com>
2022-04-15 08:04:40 -0400

    Chapter - 3  init

---
+++ 03 - Your First ASP.NET Core Application/PartyInvitesMike/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PartyInvitesMike.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
