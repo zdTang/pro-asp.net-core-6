c25820b15851b1c8edfc2f0b98fe2964b5cab4f9
Zhendong Tang <zhendongtt@gmail.com>
2022-04-17 06:57:26 -0400

    Creating Data Model

---
+++ 05 - Language Features/LanguageFeatureMike/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace LanguageFeatureMike.Models {
+
+    /// <summary>
+    /// This class is a great example for understanding Class or Type
+    /// Fields are solid within an object, which represent states
+    /// Methods are not solid which are taking care of Logics and not occupy Instance space
+    /// </summary>
+    
+    public class Product {
+
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts() {
+
+            Product kayak = new Product {
+                Name = "Kayak", Price = 275M
+            };
+
+            Product lifejacket = new Product {
+                Name = "Lifejacket", Price = 48.95M
+            };
+
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
