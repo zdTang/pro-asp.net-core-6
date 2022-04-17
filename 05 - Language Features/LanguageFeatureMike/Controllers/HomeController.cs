7075319ea72128fbfc97f95d0e03951f6d6e7f15
Zhendong Tang <zhendongtt@gmail.com>
2022-04-17 07:41:43 -0400

    Creating the controller and View

---
+++ 05 - Language Features/LanguageFeatureMike/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatureMike.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(new string[] {"c#","language","features"});
+        }
+    }
+}
