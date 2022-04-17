968a02208ee83b2634750d5bdacf5d3782b0f211
Zhendong Tang <zhendongtt@gmail.com>
2022-04-17 06:47:59 -0400

    Enabling the MVC Framework

--- 05 - Language Features/LanguageFeatureMike/Program.cs
+++ 05 - Language Features/LanguageFeatureMike/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();         // init MVC framework
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();                    // using default MVC routing method
 
 app.Run();
