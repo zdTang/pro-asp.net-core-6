86319b29b6701793b4dee450829ce1a024ac3637
Zhendong Tang <zhendongtt@gmail.com>
2022-04-22 22:12:17 -0400

    Create Data Model

---
+++ 06 - Unit Testing/Testing/SimpleApp/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }=string.Empty;
+        public decimal? Price { set; get; }
+        public static Product[] GetProducts()
+        {
+            var kayak = new Product { Name = "Kayak", Price = 275M };
+            var lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
+            return new Product[] { kayak, lifejacket }; 
+        }
+
+
+    }
+}
