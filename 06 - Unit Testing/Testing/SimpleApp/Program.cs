27405d31ecf5601379f8febb856856ce8f7a77de
Zhendong Tang <zhendongtt@gmail.com>
2022-04-22 22:06:20 -0400

    Enabling the MVC Framework
    ASP.NET Core is a platform
    MVC is a framework

--- 06 - Unit Testing/Testing/SimpleApp/Program.cs
+++ 06 - Unit Testing/Testing/SimpleApp/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();  //  Enable MVC Framework
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();   // Add Endpoint for Controller Actions
 
 app.Run();
