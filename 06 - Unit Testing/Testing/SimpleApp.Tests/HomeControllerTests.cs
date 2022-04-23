c898c0f5f4b5556c84be77ce579270d855110fc2
Zhendong Tang <zhendongtt@gmail.com>
2022-04-23 18:39:27 -0400

    Isolating a Component

--- 06 - Unit Testing/Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ 06 - Unit Testing/Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -8,20 +8,45 @@ namespace SimpleApp.Tests
 {
     public  class HomeControllerTests
     {
+        class FakeDataSource : IDataSource
+        {
+            public FakeDataSource(Product[] data) => Products = data;
+            public IEnumerable<Product> Products {get;set; }
+        }
+
+
         [Fact]
+        //public void IndexActionModelIsComplete()
+        //{
+        //    //Arrange
+        //    var controller = new HomeController();
+        //    Product[] products = new Product[]
+        //    {
+        //        new Product{Name="Kayak",Price=275M},
+        //        new Product{Name="Lifejacket",Price=48.95M}
+        //    };
+        //    //Act
+        //    var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+        //    //Assert
+        //    Assert.Equal(products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+        //}
         public void IndexActionModelIsComplete()
         {
             //Arrange
-            var controller = new HomeController();
+            
             Product[] products = new Product[]
             {
-                new Product{Name="Kayak",Price=275M},
-                new Product{Name="Lifejacket",Price=48.95M}
+                new Product{Name="p1",Price=275M},
+                new Product{Name="p2",Price=48.95M},
+                new Product{Name="p3",Price=48.95M}
             };
-            //Act
-            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+            IDataSource data = new FakeDataSource(products);
+            var controller = new HomeController();
+            controller.dataSource = data;             // Notice this is Controller's property
+            //Act  
+            var model = controller.Index()?.ViewData.Model as IEnumerable<Product>;
             //Assert
-            Assert.Equal(products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+            Assert.Equal(data.Products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
         }
     }
 }
--- 06 - Unit Testing/Testing/SimpleApp/Models/Product.cs
+++ 06 - Unit Testing/Testing/SimpleApp/Models/Product.cs
@@ -1,16 +1,27 @@
-﻿namespace SimpleApp.Models
+﻿
+
+namespace SimpleApp.Models
 {
     public class Product
     {
         public string Name { get; set; }=string.Empty;
         public decimal? Price { set; get; }
-        public static Product[] GetProducts()
-        {
-            var kayak = new Product { Name = "Kayak", Price = 275M };
-            var lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
-            return new Product[] { kayak, lifejacket }; 
-        }
+        //public static Product[] GetProducts()
+        //{
+        //    var kayak = new Product { Name = "Kayak", Price = 275M };
+        //    var lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
+        //    return new Product[] { kayak, lifejacket }; 
+        //}
+    }
 
+    public class ProductDataSource : IDataSource
+    {
+        public IEnumerable<Product> Products =>
+            new Product[] {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M }
+            };
 
+    
     }
 }
