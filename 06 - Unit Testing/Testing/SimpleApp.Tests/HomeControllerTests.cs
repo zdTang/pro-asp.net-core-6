a4945e2c7f379e537f11d9ec04478690bfe38021
Zhendong Tang <zhendongtt@gmail.com>
2022-04-23 19:03:44 -0400

    Using MOQ package

--- 06 - Unit Testing/Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ 06 - Unit Testing/Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using SimpleApp.Controllers;
 using SimpleApp.Models;
 using System.Collections.Generic;
@@ -8,11 +9,11 @@ namespace SimpleApp.Tests
 {
     public  class HomeControllerTests
     {
-        class FakeDataSource : IDataSource
-        {
-            public FakeDataSource(Product[] data) => Products = data;
-            public IEnumerable<Product> Products {get;set; }
-        }
+        //class FakeDataSource : IDataSource
+        //{
+        //    public FakeDataSource(Product[] data) => Products = data;
+        //    public IEnumerable<Product> Products {get;set; }
+        //}
 
 
         [Fact]
@@ -30,23 +31,51 @@ namespace SimpleApp.Tests
         //    //Assert
         //    Assert.Equal(products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
         //}
+
+        // Interface Version
+
+
+        //public void IndexActionModelIsComplete()
+        //{
+        //    //Arrange
+            
+        //    Product[] products = new Product[]
+        //    {
+        //        new Product{Name="p1",Price=275M},
+        //        new Product{Name="p2",Price=48.95M},
+        //        new Product{Name="p3",Price=48.95M}
+        //    };
+        //    IDataSource data = new FakeDataSource(products);
+        //    var controller = new HomeController();
+        //    controller.dataSource = data;             // Notice this is Controller's property
+        //    //Act  
+        //    var model = controller.Index()?.ViewData.Model as IEnumerable<Product>;
+        //    //Assert
+        //    Assert.Equal(data.Products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+        //}
+
+        // Mock version
+
         public void IndexActionModelIsComplete()
         {
             //Arrange
-            
+
             Product[] products = new Product[]
             {
                 new Product{Name="p1",Price=275M},
                 new Product{Name="p2",Price=48.95M},
                 new Product{Name="p3",Price=48.95M}
             };
-            IDataSource data = new FakeDataSource(products);
+
+            var mock = new Mock<IDataSource>();
+            mock.SetupGet(x => x.Products).Returns(products);   
             var controller = new HomeController();
-            controller.dataSource = data;             // Notice this is Controller's property
+            controller.dataSource = mock.Object;             // Notice this is Controller's property
             //Act  
             var model = controller.Index()?.ViewData.Model as IEnumerable<Product>;
             //Assert
-            Assert.Equal(data.Products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+            Assert.Equal(products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+            mock.VerifyGet(m=>m.Products,Times.Once);
         }
     }
 }
