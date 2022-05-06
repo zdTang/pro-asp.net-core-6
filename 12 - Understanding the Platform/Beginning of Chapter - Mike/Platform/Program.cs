a2fd60f5dcf0e037808c6336a0ab775b39ddb9fa
Mike <Zhendongtt@gmail.com>
2022-05-06 06:17:15 -0400

    [12] Creating Middleware with lambda expression
    Be aware, here we pass an asynchronous  lambda expression to the Use()

--- 12 - Understanding the Platform/Beginning of Chapter - Mike/Platform/Program.cs
+++ 12 - Understanding the Platform/Beginning of Chapter - Mike/Platform/Program.cs
@@ -5,6 +5,16 @@ var app = builder.Build();  // here will generate a WebApplication instance
 //WebApplication can be linked with a bunch of Middleware to make a request PipeLine
 
 
+// By aware, here the platform will pass a async function into the Use(), Similar with the Promise in JS
+app.Use(async (context, next) =>
+{
+    if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+    {
+        context.Response.ContentType = "text/plain";                    // assign Response Header
+        await context.Response.WriteAsync("Custom MiddleWare \n");  // Write text to Response Body
+    }
+    await next();
+});
 
 //MapGet is extension of certain Interface(IEndpointRouteBuilder) by which WebApplication inherit
 app.MapGet("/", () => "Hello World!");
