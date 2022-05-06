98c582b0edbe7e5a91dd64035716f0bfa182bc04
Mike <Zhendongtt@gmail.com>
2022-05-06 10:26:52 -0400

    [12] Study Response.ContentType order
    if add this Content on the top , then other stuff works

---
+++ 12 - Understanding the Platform/Beginning of Chapter - Mike/Platform/Middleware.cs
@@ -0,0 +1,27 @@
+﻿namespace Platform;
+
+public class QueryStringMiddleWare
+{
+    private readonly RequestDelegate _next;
+
+    public QueryStringMiddleWare(RequestDelegate nextDelegate)
+    {
+        _next = nextDelegate;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+       
+        
+        if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+        {
+            //This will cause Error as the Header has been set, and the Header has been sent yet!!! 
+            context.Response.ContentType = "text/plain";                    // assign Response Header
+            await context.Response.WriteAsync("Custom MiddleWare _ Class Approach!\n");  // Write text to Response Body
+        }
+        await _next(context);
+        await context.Response.WriteAsync("Custom MiddleWare _ Class Approach!_Return !\n");  // Write text to Response Body
+        
+    }
+
+}
--- 12 - Understanding the Platform/Beginning of Chapter - Mike/Platform/Program.cs
+++ 12 - Understanding the Platform/Beginning of Chapter - Mike/Platform/Program.cs
@@ -6,19 +6,40 @@ var app = builder.Build();  // here will generate a WebApplication instance
 
 
 // By aware, here the platform will pass a async function into the Use(), Similar with the Promise in JS
-app.Use(async (context, next) =>
-{
-    if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
-    {
-        context.Response.ContentType = "text/plain";                    // assign Response Header
-        await context.Response.WriteAsync("Custom MiddleWare \n");  // Write text to Response Body
-    }
-    await next();
-    await context.Response.WriteAsync("Mike make fun \n");  //  this line will behind "Hello World"
+//app.Use(async (context, next) =>
+//{
+//    if(context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+//    {
+//        //context.Response.ContentType = "text/plain";                    // assign Response Header
+//        await context.Response.WriteAsync("Custom MiddleWare1\n");  // Write text to Response Body
+//    }
+//    await next();
+//    await context.Response.WriteAsync("Mike make fun1\n");  //  this line will behind "Hello World"
 
-});
+//});
 
+//app.Use(async (context, next) =>
+//{
+//    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+//    {
+//        //context.Response.ContentType = "text/plain";                    // assign Response Header
+//        await context.Response.WriteAsync("Custom MiddleWare2 \n");  // Write text to Response Body
+//    }
+//    await next();
+//    await context.Response.WriteAsync("Mike make fun2 \n");  //  this line will behind "Hello World"
 
+//});
+//app.Use(async (context, next) =>
+//{
+//    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+//    {
+//        //context.Response.ContentType = "text/plain";                    // assign Response Header
+//        await context.Response.WriteAsync("Custom MiddleWare3 \n");  // Write text to Response Body
+//    }
+//    await next();
+//    await context.Response.WriteAsync("Mike make fun2 \n");  //  this line will behind "Hello World"
+
+//});
 
 app.Use(async (context, next) =>
 {
@@ -26,12 +47,40 @@ app.Use(async (context, next) =>
     {
         //This will cause Error as the Header has been set, and the Header has been sent yet!!! 
         context.Response.ContentType = "text/plain";                    // assign Response Header
-        await context.Response.WriteAsync("Custom MiddleWare Two!\n");  // Write text to Response Body
+        await context.Response.WriteAsync("Custom MiddleWare 4!\n");  // Write text to Response Body
     }
     await next();
-    await context.Response.WriteAsync("Mike make fun Two!\n");  //  this line will behind "Hello World"
+    await context.Response.WriteAsync("Mike make fun 4!\n");  //  this line will behind "Hello World"
 
 });
+
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+    {
+        //context.Response.ContentType = "text/plain";                    // assign Response Header
+        await context.Response.WriteAsync("Custom MiddleWare3 \n");  // Write text to Response Body
+    }
+    await next();
+    await context.Response.WriteAsync("Mike make fun3 \n");  //  this line will behind "Hello World"
+
+});
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+    {
+        //context.Response.ContentType = "text/plain";                    // assign Response Header
+        await context.Response.WriteAsync("Custom MiddleWare1 \n");  // Write text to Response Body
+    }
+    await next();
+    await context.Response.WriteAsync("Mike make fun1 \n");  //  this line will behind "Hello World"
+
+});
+
+
+
 
 //MapGet is extension of certain Interface(IEndpointRouteBuilder) by which WebApplication inherit
 app.MapGet("/", () => "Hello World! \n");
