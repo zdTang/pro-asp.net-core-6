a2b5ed43532eb2fc71c03c848e7b9974a4a3cd77
Mike <Zhendongtt@gmail.com>
2022-05-06 05:52:13 -0400

    [12] Understanding the entry point

--- 12 - Understanding the Platform/Beginning of Chapter - Mike/Platform/Program.cs
+++ 12 - Understanding the Platform/Beginning of Chapter - Mike/Platform/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+//Builder is used for configuration 
 
+var app = builder.Build();  // here will generate a WebApplication instance
+//WebApplication can be linked with a bunch of Middleware to make a request PipeLine
+
+
+
+//MapGet is extension of certain Interface(IEndpointRouteBuilder) by which WebApplication inherit
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
