d38ef3d6a6913946e6354867237026f22e9b0d6f
Mike <Zhendongtt@gmail.com>
2022-05-07 10:08:29 -0400

    [30] This approach duplicate code and not good

--- 30 - Filters/Beginning of Chapter - Mike/WebApp/Controllers/HomeController.cs
+++ 30 - Filters/Beginning of Chapter - Mike/WebApp/Controllers/HomeController.cs
@@ -17,4 +17,21 @@ public class HomeController : Controller
             return new StatusCodeResult(StatusCodes.Status403Forbidden);
         }
     }
+    
+    /// <summary>
+    /// Check HTTPS using this approach will duplicate code in every action
+    /// </summary>
+    /// <returns></returns>
+    public IActionResult Secure()
+    {
+        if (Request.IsHttps)
+        {
+            return View("Message","This is the Secure action on the Home Controller");
+        }
+        else
+        {
+            //the response will have no body, only header 403 Forbidden
+            return new StatusCodeResult(StatusCodes.Status403Forbidden);
+        }
+    }
 }
