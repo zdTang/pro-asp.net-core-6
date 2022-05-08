1b82e50c623e61715ed8f17e9e5df2b149433f5a
Mike <Zhendongtt@gmail.com>
2022-05-08 09:45:33 -0400

    [30] Create Asynchronous Resource Filter
    Be aware, this filter can be put on Controller and action level or global level
    
    The scope of filter has no relationship of the category of filter.   Each category filter has different Scope

--- 30 - Filters/Beginning of Chapter - Mike/WebApp/Controllers/HomeController.cs
+++ 30 - Filters/Beginning of Chapter - Mike/WebApp/Controllers/HomeController.cs
@@ -7,15 +7,16 @@ namespace WebApp;
 /// Different level of filter!! Controller level
 /// </summary>
 
+[SimpleCacheAsync]
 public class HomeController : Controller
 {    
   
-    [HttpsOnly]
+    //[HttpsOnly]
+    //[SimpleCache]
+    
     public IActionResult Index()
     {
-       
-            return View("Message","This is the Index action on the Home Controller");
-
+       return View("Message","This is the Index action on the Home Controller");
     }
     
 
---
+++ 30 - Filters/Beginning of Chapter - Mike/WebApp/Filters/SimpleCacheAsyncAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters;
+
+public class SimpleCacheAsyncAttribute:Attribute,IAsyncResourceFilter
+{
+    private Dictionary<PathString, IActionResult> CachedResponses = new Dictionary<PathString, IActionResult>();
+    
+    public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+    {
+        PathString path = context.HttpContext.Request.Path;
+        if (CachedResponses.ContainsKey(path))
+        {
+            //Notice: This Result is ActionResult, will be invocated to generate a final Response
+            context.Result = CachedResponses[path];
+            CachedResponses.Remove(path);
+        }
+        else
+        {
+            //Be aware ! Once the Execution come back, it will bring a new Context
+            //We can short circuit the Pipeline by Providing a Result for the new Context
+            ResourceExecutedContext executedContext = await next();
+            
+            if (executedContext.Result != null)
+            {
+                CachedResponses.Add(executedContext.HttpContext.Request.Path,executedContext.Result);
+            }
+        }
+    }
+}
