0d41b312572bbbcfba5e5c49c843be45734690f1
Mike <Zhendongtt@gmail.com>
2022-05-14 17:16:15 -0400

    [14] Create a non-static Endpoint with Dependency Injection
    Notice, in the static Endpoint: WeatherEndpoint, the Dependency is injected from Method. (Even we can inject from constructor, the static method cannot use instance element  )
    While in the Non-static Weather Endpoint, the dependency is injected from Constructor

---
+++ 14 - Dependency Injection/Mike Test/Platform/WeatherEndpointNoStatic.cs
@@ -0,0 +1,23 @@
+﻿using Platform.Services;
+
+namespace Platform {
+    public class WeatherEndpointNoStatic
+    {
+
+        /// <summary>
+        /// This one using Instance other than Static method.
+        /// Refer to WeatherEndpoint class
+        /// </summary>
+        private readonly IResponseFormatter _formatter;
+
+        public WeatherEndpointNoStatic(IResponseFormatter responseFormatter)
+        {
+            _formatter = responseFormatter;
+        }
+        public async Task Endpoint(HttpContext context)
+        {
+            
+            await _formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
+        }
+    }
+}
