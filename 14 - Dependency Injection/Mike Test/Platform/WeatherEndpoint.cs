305f3a0761d0f82f7a6ed34a46f73751d73dea9a
Mike <Zhendongtt@gmail.com>
2022-05-14 12:59:36 -0400

    [14] Get Service from HttpContext Object

--- 14 - Dependency Injection/Mike Test/Platform/WeatherEndpoint.cs
+++ 14 - Dependency Injection/Mike Test/Platform/WeatherEndpoint.cs
@@ -1,9 +1,14 @@
-﻿namespace Platform {
+﻿using Platform.Services;
+
+namespace Platform {
     public class WeatherEndpoint {
 
-        public static async Task Endpoint(HttpContext context) {
-            await context.Response
-                .WriteAsync("Endpoint Class: It is cloudy in Milan");
+        public static async Task Endpoint(HttpContext context)
+        {
+            // Access Service by HttpContext
+            IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+            //await context.Response.WriteAsync("Endpoint Class: It is cloudy in Milan");
+            await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
         }
     }
 }
