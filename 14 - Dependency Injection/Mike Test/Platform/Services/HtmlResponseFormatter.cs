10dba7110768c6eecd980fbf977c8b55e9169438
Mike <Zhendongtt@gmail.com>
2022-05-14 11:57:38 -0400

    [14] Using a Service in a Middleware Class
    1.  Once Response ContentType has been assigned, the Header will be sent
    2, Once Header has been send, we are not allowed to write to the Response body again.

--- 14 - Dependency Injection/Mike Test/Platform/Services/HtmlResponseFormatter.cs
+++ 14 - Dependency Injection/Mike Test/Platform/Services/HtmlResponseFormatter.cs
@@ -4,7 +4,7 @@ public class HtmlResponseFormatter:IResponseFormatter
 {
     public async Task Format(HttpContext context, string content)
     {
-        context.Request.ContentType = "text/html";
+        context.Response.ContentType = "text/html";
         await context.Response.WriteAsync($@"
             <!DOCTYPE html>
             <html lang=""en"">
--- 14 - Dependency Injection/Mike Test/Platform/WeatherMiddleware.cs
+++ 14 - Dependency Injection/Mike Test/Platform/WeatherMiddleware.cs
@@ -1,17 +1,23 @@
-﻿namespace Platform {
+﻿using Platform.Services;
+
+namespace Platform {
     public class WeatherMiddleware {
         private RequestDelegate next;
-
-        public WeatherMiddleware(RequestDelegate nextDelegate) {
+        private IResponseFormatter _formatter;
+        public WeatherMiddleware(RequestDelegate nextDelegate,IResponseFormatter formatter) {
             next = nextDelegate;
+            _formatter = formatter;
         }
 
         public async Task Invoke(HttpContext context) {
-            if (context.Request.Path == "/middleware/class") {
-                await context.Response
-                    .WriteAsync("Middleware Class: It is raining in London");
-            } else {
+            if (context.Request.Path == "/middleware/class")
+            {
+                await _formatter.Format(context, "Middleware Class: It is raining in London");
+            }
+            else {
                 await next(context);
+                // If Respond Header has been sent, then it will cause Error here.
+                //await _formatter.Format(context, "Middleware Class: It is raining in London again!");
             }
         }
     }
